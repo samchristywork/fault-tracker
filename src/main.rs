00c203e4216c3a7d1b0ab5352824349d66e1f251
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-04 20:13:39 -0600

    Feature: Added time parsing
    Added some very basic code to parse time strings from a line in a log
    file.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+use chrono::NaiveDateTime;
 use log::info;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub mod listen;
+
+fn extract_time(input: &str) -> Result<String, &str> {
+    let foo = input.split(|e| e == '[' || e == ']');
+    for bar in foo {
+        match NaiveDateTime::parse_from_str(bar, "%Y-%m-%dT%H:%M:%S") {
+            Ok(a) => return Ok(a.to_string()),
+            Err(_) => {}
+        }
+        match bar.trim().parse::<f64>() {
+            Ok(a) => return Ok(a.to_string()),
+            Err(_) => {}
+        }
+    }
+
+    Err(input)
+}
+
+fn handle_line(line: &str) {
+    match extract_time(line) {
+        Ok(a) => println!("{}", a),
+        Err(a) => eprintln!("{}", a),
+    }
+}
 
 fn main() {
     log4rs::init_file("src/log.yaml", Default::default()).unwrap();
